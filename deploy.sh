83b605ab377da071d75faa2f1773886bcf4be21a
Jim Nielsen <jimniels@gmail.com>
2015-01-09 11:53:49 -0700

    remove svgo optimization from deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ echo "$output" > index.html;
 echo '-> Compile CSS'
 compass compile
 
-echo '-> Compress svgs'
-find ./img/build/ -type d | while read d; do
-   # https://github.com/svg/svgo/issues/217
-   svgo -f $d/ --disable=removeUnknownsAndDefaults
-done
+# echo '-> Compress svgs'
+# find ./img/build/ -type d | while read d; do
+#    # https://github.com/svg/svgo/issues/217
+#    svgo -f $d/ --disable=removeUnknownsAndDefaults
+# done
