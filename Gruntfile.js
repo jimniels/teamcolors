12c689ddf37c5760e1f51d107eb369928f1b1475
Jim Nielsen <jimniels@gmail.com>
2015-05-08 16:24:07 -0600

    a bunch of changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,48 @@
 module.exports = function(grunt) {
     
+    // Configure paths to assets
+    var config = {
+        all: 'assets',
+        js: {
+            src: 'src/js',
+            build: 'assets/js'
+        },
+        css: {
+            src: 'src/css',
+            build: 'assets/css'
+        },
+        img: {
+            src: 'src/img',
+            build: 'assets/img'
+        },
+        data: {
+            src: 'src/data',
+            build: 'assets/data'
+        },
+        hbs: 'src/handlebars'
+    };
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        
+        config: config,
+
         // Regular tasks
+        clean: {
+            options: {
+                //'no-write': true
+            },
+            build: ['<%= config.all %>'],
+            css: ['<%= config.css.build %>'],
+            js: ['<%= config.js.build %>'],
+            data: ['<%= config.data.build %>']
+        },
+
         compass: {
-            dist: { 
+            build: { 
                 options: {
-                    basePath: 'assets/css',
-                    sassDir: 'src',
-                    cssDir: 'build',
-                    imagesDir: '../img/build',
+                    sassDir: '<%= config.css.src %>',
+                    cssDir: '<%= config.css.build %>',
+                    imagesDir: '<%= config.img.build %>',
                     outputStyle: 'compressed',
                     relativeAssets: true,
                     force: true
@@ -22,42 +54,62 @@ module.exports = function(grunt) {
             options: {
                 separator: ';\n',
             },
-            dist: {
-                src: ['assets/js/src/*'],
-                dest: 'assets/js/build/js-concat.js',
+            build: {
+                src: ['<%= config.js.src %>/*'],
+                dest: '<%= config.js.build %>/scripts.js',
             }
         },
 
         uglify: {
-            js: {
+            build: {
                 files: {
-                    'assets/js/build/js-min.js' : ['assets/js/build/js-concat.js']
+                    '<%= config.js.build %>/scripts.min.js' : 
+                    ['<%= config.js.build %>/scripts.js']
                 }
             }
         },
 
         'merge-json': {
-            teamColors: {
-                src: 'assets/data/src/*',
-                dest: 'assets/data/build/team-colors.json'
+            build: {
+                src: '<%= config.data.src %>/*',
+                dest: '<%= config.data.build %>/team-colors.json'
             }
         },
 
         'compile-handlebars': {
-            allStatic: {
-                template: 'assets/templates/index.hbs',
-                templateData: 'assets/data/build/team-colors-colorfied.json',
+            build: {
+                template: '<%= config.hbs %>/index.hbs',
+                templateData: '<%= config.data.build %>/team-colors-colorfied.json',
                 output: '*.html',
-                helpers: 'assets/templates/helpers/*.js',
-                partials: 'assets/templates/partials/*.hbs'
+                helpers: '<%= config.hbs %>/helpers/*.js',
+                partials: '<%= config.hbs %>/partials/*.hbs'
             }
         },
 
         jshint: {
-            all: [
-                'assets/js/src/js.js', 
-                'assets/data/src/*'
+            build: [
+                '<%= config.js.src %>/js.js', 
+                '<%= config.data.src %>/*'
             ]
+        },
+
+        svgmin: {
+            options: {
+                // have to do this
+                // https://github.com/svg/svgo/issues/352
+                plugins: [{mergePaths: false}]
+            },
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= config.img.src %>/',
+                        src: ['**/*.svg'],
+                        dest: '<%= config.img.build %>/',
+                        ext: '.min.svg'
+                    }
+                ]
+            }
         },
 
         // Watch Tasks
@@ -68,29 +120,33 @@ module.exports = function(grunt) {
             },
             jshint: {
                 files: [
-                    'assets/js/src/js.js',
-                    'assets/data/src/*'
+                    '<%= config.js.src %>/js.js',
+                    '<%= config.data.src %>/*'
                 ],
                 tasks: ['jshint', 'merge-json', 'colorify']
             },
             scripts: {
-                files: ['assets/js/src/*'],
-                tasks: ['jshint', 'concat', 'uglify']
+                files: ['<%= config.js.src %>/*'],
+                tasks: ['clean:js', 'jshint', 'concat', 'uglify']
             },
             colorify: {
-                files: ['assets/grunt-tasks/colorify/**/*'],
+                files: ['src/grunt-tasks/colorify/**/*'],
                 tasks: ['colorify']
             },
             handlebars: {
                 files: [
-                    'assets/templates/**/*', 
-                    'assets/data/build/team-colors-colorfied.json'
+                    '<%= config.hbs %>/**/*', 
+                    '<%= config.data.build %>/team-colors-colorfied.json'
                 ],
                 tasks: ['compile-handlebars']
             },
             compass: {
-                files: ['assets/css/src/*'],
-                tasks: ['compass']
+                files: ['<%= config.css.src %>/*'],
+                tasks: ['clean:css', 'compass']
+            },
+            svgmin: {
+                files: ['<%= config.img.src %>/**/*'],
+                tasks: ['svgmin']
             }
         }
     });
@@ -103,17 +159,21 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-merge-json');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadTasks('assets/grunt-tasks/colorify');
+    grunt.loadNpmTasks('grunt-svgmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadTasks('src/grunt-tasks/colorify');
     
     // Default task(s).
     grunt.registerTask('default', [
+        'clean',
         'jshint',
         'concat',
         'uglify',
         'merge-json',
         'colorify',
         'compile-handlebars', 
-        'compass', 
+        'svgmin',
+        'compass',
         'watch'
     ]);
 };
